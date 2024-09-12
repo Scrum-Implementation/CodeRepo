b1d0e59b4eedeea1fe5ba37c69d9c8ff6a235b31
Gargan07 <gargantillaeugene02@gmail.com>
2024-09-12 23:25:42 +0800

    modified the connection part

--- taskmanagement.php
+++ taskmanagement.php
@@ -1,15 +1,6 @@
 <?php
 
-$servername = "localhost:3306";
-$username = "root";
-$password = "";
-$dbname = "todo_db";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include 'connection.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
