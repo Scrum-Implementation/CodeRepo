cbfc0aa3a47c38909b191337850c1e7d5f6baef6
Gargan07 <gargantillaeugene02@gmail.com>
2024-09-15 16:58:48 +0800

    fix discrepancies and add the 'status'' business logic

--- fetchdata.php
+++ fetchdata.php
@@ -1,4 +1,5 @@
 <?php
+
 include 'connection.php';
 
 $sql = "SELECT * FROM todos";
@@ -6,9 +7,23 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
+
+        $end_time = date("H:i", strtotime($row["end_time"]));
+        $due_date_and_time = new DateTime($row["due_date"] . " " . $end_time, new DateTimeZone('Asia/Manila'));
+        $today = new DateTime('now', new DateTimeZone('Asia/Manila'));
+
+        if ($due_date_and_time > $today) {
+            $status = "In Progress";
+        } else {
+            $status = "Completed";
+        }
+
+        $sql = "UPDATE todos SET status='$status' WHERE id=".$row["id"];
+        $conn->query($sql);
+           
         echo "<tr>
-        <td>" . $row["name"] . "<br>" . $row["description"] ."</td>;
-        <td>" . $row["due_date"] . "</td>
+        <td>" . $row["name"] . "<br>" . $row["description"] ."</td>
+        <td>" . $row["due_date"] . "<br>" . $end_time . "</td>
         <td>" . $row["status"] . "</td>
         </tr>";
     }
